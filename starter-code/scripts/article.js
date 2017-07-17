4e69b431d7a60203aec092f702cc0f6f0578af90
ByteMiBro <bytemibro@gmail.com>
2017-07-17 12:52:42 -0600

    Console.log to check if local storage is working

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -54,6 +54,7 @@ Article.fetchAll = function() {
     //TODO: What method do we call to render the index page?
     articleView.initIndexPage();
   } else {
+    console.log("Local storage data not found");
     // TODO: When we don't already have the rawData,
     // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
