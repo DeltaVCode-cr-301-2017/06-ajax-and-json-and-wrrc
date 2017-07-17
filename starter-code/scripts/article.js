094f5ea5a0e391623d070989b6cffcbacebfb420
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-07-17 13:42:56 -0500

    cleanup

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -58,10 +58,5 @@ Article.fetchAll = function() {
       Article.loadAll(data);
       articleView.initIndexPage();
     });
-    // TODO: When we don't already have the rawData,
-    // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
-    // cache it in localStorage so we can skip the server call next time,
-    // then load all the data into Article.all with the .loadAll function above,
-    // and then render the index page.
   }
 }
